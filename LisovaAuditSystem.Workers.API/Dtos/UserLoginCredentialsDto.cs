e745f4f00a9e9657481920e47a762faf21341a1b
Vitalii Barabash <vitalik.barabash.2002@gmail.com>
2025-02-15 20:14:49 +0200

    defined two credentials transferring object

---
+++ LisovaAuditSystem.Workers.API/Dtos/UserLoginCredentialsDto.cs
@@ -0,0 +1,8 @@
+﻿namespace LisovaAuditSystem.Workers.API.Dtos;
+
+public class UserLoginCredentialsDto(string email, string password)
+{
+    public string Email { get; set; } = email;
+
+    public string Password { get; set; } = password;
+}
---
+++ LisovaAuditSystem.Workers.API/Dtos/UserRegisterCredentialsDto.cs
@@ -0,0 +1,10 @@
+﻿namespace LisovaAuditSystem.Workers.API.Dtos;
+
+public class UserRegisterCredentialsDto(string email, string username, string password)
+{
+    public string UserName { get; set; } = username;
+
+    public string Email { get; set; } = email;
+
+    public string Password { get; set; } = password;
+}
