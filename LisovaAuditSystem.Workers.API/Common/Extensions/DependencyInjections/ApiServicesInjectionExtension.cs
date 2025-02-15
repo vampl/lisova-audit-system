97d0b67962ee7e6621eb3d616dca24593d413f56
Vitalii Barabash <vitalik.barabash.2002@gmail.com>
2025-02-15 14:51:30 +0200

    defined and implemented DI extension, added transient configuration model, configured authentication options

---
+++ LisovaAuditSystem.Workers.API/Common/Extensions/DependencyInjections/ApiServicesInjectionExtension.cs
@@ -0,0 +1,71 @@
+﻿using System.Text;
+
+using LisovaAuditSystem.Workers.API.Common.Configurations;
+
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace LisovaAuditSystem.Workers.API.Common.Extensions.DependencyInjections;
+
+public static class ApiServicesInjectionExtension
+{
+    public static void AddApiServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddTransient<JwtConfiguration>();
+
+        services.AddAuthentication(
+                options =>
+                {
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+            .AddJwtBearer(
+                options =>
+                {
+                    var jwtConfiguration = new JwtConfiguration(configuration);
+
+                    options.SaveToken = true;
+
+                    options.TokenValidationParameters =
+                        new TokenValidationParameters
+                        {
+                            ValidateIssuer = true,
+                            ValidateAudience = true,
+                            ValidateLifetime = true,
+                            ValidateIssuerSigningKey = true,
+
+                            ValidIssuer = jwtConfiguration.Issuer,
+                            ValidAudience = jwtConfiguration.Audience,
+                            IssuerSigningKey =
+                                new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtConfiguration.Secret)),
+
+                            RequireExpirationTime = true
+                        };
+
+                    options.Events =
+                        new JwtBearerEvents
+                        {
+                            OnMessageReceived =
+                                httpContext =>
+                                {
+                                    string? authorization = httpContext.Request.Headers.Authorization;
+
+                                    if (string.IsNullOrEmpty(authorization))
+                                    {
+                                        httpContext.NoResult();
+                                    }
+                                    else
+                                    {
+                                        httpContext.Token = authorization.Replace("Bearer ", string.Empty);
+                                    }
+
+                                    return Task.CompletedTask;
+                                }
+                        };
+                });
+        services.AddAuthorization();
+
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+    }
+}
