c6fb15b5c682eb9ca124e7a7bad4ca8ba21c9b7c
Vitalii Barabash <vitalik.barabash.2002@gmail.com>
2025-02-15 17:58:54 +0200

    defined UserDto for data transfer purposes

---
+++ LisovaAuditSystem.Workers.API/Dtos/UserDto.cs
@@ -0,0 +1,12 @@
+﻿namespace LisovaAuditSystem.Workers.API.Dtos;
+
+public class UserDto(Guid id, string userName, string email, string passwordHash)
+{
+    public Guid Id { get; set; } = id;
+
+    public string UserName { get; set; } = userName;
+
+    public string Email { get; set; } = email;
+
+    public string PasswordHash { get; set; } = passwordHash;
+}
