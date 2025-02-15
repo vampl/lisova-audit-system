621f1d4f61acd4fe83dc1461e190813799da7237
Vitalii Barabash <vitalik.barabash.2002@gmail.com>
2025-02-15 17:57:07 +0200

    defined and implemented User model mapping unig tests

---
+++ LisovaAuditSystem.Workers.Mapping.Tests/UserMappingTests.cs
@@ -0,0 +1,60 @@
+﻿using FluentAssertions;
+
+using LisovaAuditSystem.Workers.API.Common.Extensions.Mappings;
+using LisovaAuditSystem.Workers.API.Dtos;
+using LisovaAuditSystem.Workers.API.Entities;
+
+namespace LisovaAuditSystem.Workers.Mapping.Tests;
+
+public class UserMappingTests
+{
+    [Fact]
+    public void ToDto_WhenCalledOnEntity_ReturnsDto()
+    {
+        // Arrange
+        User user =
+            new(
+                id: Guid.NewGuid(),
+                userName: "Joe_Doe",
+                email: "joe.doe@gmail.com",
+                passwordHash: "47D12392-B623-4942-B919-4E713BB4F365");
+
+        UserDto expected =
+            new(
+                id: user.Id,
+                userName: user.UserName,
+                email: user.Email,
+                passwordHash: user.PasswordHash);
+
+        // Act
+        UserDto actual = user.ToDto();
+
+        // Assert
+        actual.Should().BeEquivalentTo(expected);
+    }
+
+    [Fact]
+    public void ToEntity_WhenCalledOnDto_ReturnsEntity()
+    {
+        // Arrange
+        UserDto user =
+            new(
+                id: Guid.NewGuid(),
+                userName: "Joe_Doe",
+                email: "joe.doe@gmail.com",
+                passwordHash: "47D12392-B623-4942-B919-4E713BB4F365");
+
+        User expected =
+            new(
+                id: user.Id,
+                userName: user.UserName,
+                email: user.Email,
+                passwordHash: user.PasswordHash);
+
+        // Act
+        User actual = user.ToEntity();
+
+        // Assert
+        actual.Should().BeEquivalentTo(expected);
+    }
+}
