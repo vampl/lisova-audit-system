e56b63a308dc19bca8bc8ef037235134fa7b8a8c
Vitalii Barabash <vitalik.barabash.2002@gmail.com>
2025-02-15 15:14:22 +0200

    defined and implemented Jwt token generation service

---
+++ LisovaAuditSystem.Workers.API/Services/Authentication/JwtTokenGenerationService.cs
@@ -0,0 +1,36 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+using LisovaAuditSystem.Workers.API.Common.Configurations;
+
+using Microsoft.IdentityModel.Tokens;
+
+namespace LisovaAuditSystem.Workers.API.Services.Authentication;
+
+public class JwtTokenGenerationService(JwtConfiguration configuration)
+{
+    public string Generate(Guid userId, string userName, string userEmail)
+    {
+        Claim[] claims =
+        [
+            new(JwtRegisteredClaimNames.Sub, userId.ToString()),
+            new(JwtRegisteredClaimNames.UniqueName, userName),
+            new(JwtRegisteredClaimNames.Email, userEmail)
+        ];
+
+        SymmetricSecurityKey key = new(Encoding.UTF8.GetBytes(configuration.Secret));
+        SigningCredentials credentials = new(key, SecurityAlgorithms.HmacSha256);
+
+        JwtSecurityToken token =
+            new(
+                issuer: configuration.Issuer,
+                audience: configuration.Audience,
+                claims: claims,
+                expires: DateTime.Now.AddDays(configuration.ExpireDays),
+                signingCredentials: credentials
+            );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
