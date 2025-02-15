eb714140f9c1f0a137a973b12e2921e7b274a563
Vitalii Barabash <vitalik.barabash.2002@gmail.com>
2025-02-15 20:12:05 +0200

    defined and implemented password hashing service

---
+++ LisovaAuditSystem.Workers.API/Services/PasswordHasher.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Cryptography;
+
+using LisovaAuditSystem.Workers.API.Interfaces.Services;
+
+namespace LisovaAuditSystem.Workers.API.Services;
+
+public class PasswordHasher : IPasswordHasher
+{
+    private const int SaltSize = 16;
+    private const int HashSize = 32;
+
+    private const int HashingIterations = 100000;
+
+    private static readonly HashAlgorithmName Algorithm = HashAlgorithmName.SHA512;
+
+    public string Hash(string password)
+    {
+        byte[] salt = RandomNumberGenerator.GetBytes(SaltSize);
+        byte[] hash = Rfc2898DeriveBytes.Pbkdf2(password, salt, HashingIterations, Algorithm, HashSize);
+
+        return $"{Convert.ToHexString(hash)}-{Convert.ToHexString(salt)}";
+    }
+
+    public bool Verify(string password, string passwordHash)
+    {
+        string[] parts = passwordHash.Split('-');
+
+        byte[] hash = Convert.FromHexString(parts[0]);
+        byte[] salt = Convert.FromHexString(parts[1]);
+
+        byte[] inputHash = Rfc2898DeriveBytes.Pbkdf2(password, salt, HashingIterations, Algorithm, HashSize);
+
+        return hash.SequenceEqual(inputHash);
+    }
+}
