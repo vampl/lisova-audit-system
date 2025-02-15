0953f610b9ec795d0f3e8a178753c8e62460bc9e
Vitalii Barabash <vitalik.barabash.2002@gmail.com>
2025-02-15 17:58:13 +0200

    defined User entity

---
+++ LisovaAuditSystem.Workers.API/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿namespace LisovaAuditSystem.Workers.API.Entities;
+
+public class User(Guid id, string userName, string email, string passwordHash)
+{
+    public Guid Id { get; private set; } = id;
+
+    public string UserName { get; private set; } = userName;
+
+    public string Email { get; private set; } = email;
+
+    public string PasswordHash { get; private set; } = passwordHash;
+}
