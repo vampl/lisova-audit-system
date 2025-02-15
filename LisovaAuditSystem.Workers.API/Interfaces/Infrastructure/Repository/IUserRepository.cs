06503da76eb8cc52183ef9cb3ef6b7a914804738
Vitalii Barabash <vitalik.barabash.2002@gmail.com>
2025-02-15 18:03:03 +0200

    defined user-specific repository with inheritance from general contract and additional methods

---
+++ LisovaAuditSystem.Workers.API/Interfaces/Infrastructure/Repository/IUserRepository.cs
@@ -0,0 +1,8 @@
+﻿using LisovaAuditSystem.Workers.API.Dtos;
+
+namespace LisovaAuditSystem.Workers.API.Interfaces.Infrastructure.Repository;
+
+public interface IUserRepository : IRepository<UserDto>
+{
+    Task<UserDto?> GetByEmailAsync(string email, bool asNoTracking = true);
+}
