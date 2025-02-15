6132432d2e0a04b71b0399e0c35da46966697290
Vitalii Barabash <vitalik.barabash.2002@gmail.com>
2025-02-15 18:00:36 +0200

    defined User's type set, configured User's type entity

--- LisovaAuditSystem.Workers.API/Infrastructure/WorkersContext.cs
+++ LisovaAuditSystem.Workers.API/Infrastructure/WorkersContext.cs
@@ -8,6 +8,8 @@ public class WorkersContext(DbContextOptions<WorkersContext> options) : DbContex
 {
     public DbSet<Worker> Workers { get; set; }
 
+    public DbSet<User> Users { get; set; }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Worker>(
@@ -41,6 +43,33 @@ public class WorkersContext(DbContextOptions<WorkersContext> options) : DbContex
                 builder.Property(worker => worker.Email)
                     .HasMaxLength(maxEmailLength)
                     .IsRequired(false);
+            });
+
+        modelBuilder.Entity<User>(
+            builder =>
+            {
+                const int maxUserNameLength = 16;
+                const int maxEmailLength = 256;
+                const int maxPasswordLength = 512;
+
+                builder.ToTable("Users");
+
+                builder.HasKey(user => user.Id);
+
+                builder.Property(user => user.UserName)
+                    .HasMaxLength(maxUserNameLength)
+                    .IsRequired();
+
+                builder.Property(user => user.Email)
+                    .HasMaxLength(maxEmailLength)
+                    .IsRequired();
+
+                builder.Property(user => user.PasswordHash)
+                    .HasMaxLength(maxPasswordLength)
+                    .IsRequired();
+
+                builder.HasIndex(user => user.UserName).IsUnique();
+                builder.HasIndex(user => user.Email).IsUnique();
             });
 
         base.OnModelCreating(modelBuilder);
