9914c4565bdf73b689d810f945329b1f2ca87916
Vitalii Barabash <vitalik.barabash.2002@gmail.com>
2025-02-15 17:59:31 +0200

    defined and implemented to/from entity mapping methods

---
+++ LisovaAuditSystem.Workers.API/Common/Extensions/Mappings/UserMappingExtension.cs
@@ -0,0 +1,25 @@
+﻿using LisovaAuditSystem.Workers.API.Dtos;
+using LisovaAuditSystem.Workers.API.Entities;
+
+namespace LisovaAuditSystem.Workers.API.Common.Extensions.Mappings;
+
+public static class UserMappingExtension
+{
+    public static UserDto ToDto(this User user)
+    {
+        return new UserDto(
+            id: user.Id,
+            userName: user.UserName,
+            email: user.Email,
+            passwordHash: user.PasswordHash);
+    }
+
+    public static User ToEntity(this UserDto user)
+    {
+        return new User(
+            id: user.Id,
+            userName: user.UserName,
+            email: user.Email,
+            passwordHash: user.PasswordHash);
+    }
+}
