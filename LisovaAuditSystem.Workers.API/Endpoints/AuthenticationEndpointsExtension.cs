d0390751a4ee8df418734889e1be84a591235fb3
Vitalii Barabash <vitalik.barabash.2002@gmail.com>
2025-02-15 20:27:19 +0200

    defined and implemented authentication endpoints

---
+++ LisovaAuditSystem.Workers.API/Endpoints/AuthenticationEndpointsExtension.cs
@@ -0,0 +1,80 @@
+﻿using LisovaAuditSystem.Workers.API.Dtos;
+using LisovaAuditSystem.Workers.API.Interfaces.Services;
+using LisovaAuditSystem.Workers.API.Models.Requests;
+
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace LisovaAuditSystem.Workers.API.Endpoints;
+
+public static class AuthenticationEndpointsExtension
+{
+    const int ProblemHttpResultStatusCode = 500;
+
+    public static void MapAuthenticationEndpoints(this IEndpointRouteBuilder routes)
+    {
+        RouteGroupBuilder group = routes.MapGroup("api/v1/authentications");
+
+        group.MapPost("register", PostUserRegistration)
+            .AllowAnonymous();
+        group.MapPost("login", PostUserLogin)
+            .AllowAnonymous();
+    }
+
+    private static async Task<Results<Ok<string>, BadRequest<string>, ProblemHttpResult>> PostUserRegistration(
+        IAuthenticationService authenticationService,
+        PostUserRegistrationRequest request)
+    {
+        try
+        {
+            return TypedResults.Ok(
+                await authenticationService.RegisterAsync(
+                    new UserRegisterCredentialsDto(
+                        username: request.PostUserRegistrationPayload.UserName,
+                        email: request.PostUserRegistrationPayload.Email,
+                        password: request.PostUserRegistrationPayload.Password
+                    )));
+        }
+        catch (InvalidOperationException exception) when (exception.Message.Contains("already exists"))
+        {
+            return TypedResults.BadRequest(exception.Message);
+        }
+        catch (InvalidOperationException exception)
+        {
+            return TypedResults.BadRequest(exception.Message);
+        }
+        catch (Exception exception)
+        {
+            return TypedResults.Problem(detail: exception.Message, statusCode: ProblemHttpResultStatusCode);
+        }
+    }
+
+    private static async Task<Results<Ok<string>, BadRequest<string>, NotFound<string>, ProblemHttpResult>> PostUserLogin(
+        IAuthenticationService authenticationService,
+        PostUserLoginRequest request)
+    {
+        try
+        {
+            return TypedResults.Ok(
+                await authenticationService.LoginAsync(
+                    new UserLoginCredentialsDto(
+                        email: request.PostUserLoginPayload.Email,
+                        password: request.PostUserLoginPayload.Password)));
+        }
+        catch (InvalidOperationException exception) when (exception.Message.Contains("already exists"))
+        {
+            return TypedResults.BadRequest(exception.Message);
+        }
+        catch (InvalidOperationException exception) when (exception.Message.Contains("not found"))
+        {
+            return TypedResults.NotFound(exception.Message);
+        }
+        catch (InvalidOperationException exception)
+        {
+            return TypedResults.BadRequest(exception.Message);
+        }
+        catch (Exception exception)
+        {
+            return TypedResults.Problem(detail: exception.Message, statusCode: ProblemHttpResultStatusCode);
+        }
+    }
+}
