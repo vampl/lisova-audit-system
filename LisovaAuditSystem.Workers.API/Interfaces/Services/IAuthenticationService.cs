0531763466c57cdabf001f3e6921033de51bf900
Vitalii Barabash <vitalik.barabash.2002@gmail.com>
2025-02-15 20:14:03 +0200

    defined and implemented authentication service

---
+++ LisovaAuditSystem.Workers.API/Interfaces/Services/IAuthenticationService.cs
@@ -0,0 +1,10 @@
+﻿using LisovaAuditSystem.Workers.API.Dtos;
+
+namespace LisovaAuditSystem.Workers.API.Interfaces.Services;
+
+public interface IAuthenticationService
+{
+    Task<string> RegisterAsync(UserRegisterCredentialsDto userRegisterCredentials);
+
+    Task<string> LoginAsync(UserLoginCredentialsDto userLoginCredentials);
+}
---
+++ LisovaAuditSystem.Workers.API/Services/AuthenticationService.cs
@@ -0,0 +1,52 @@
+﻿using LisovaAuditSystem.Workers.API.Dtos;
+using LisovaAuditSystem.Workers.API.Interfaces.Infrastructure.Repository;
+using LisovaAuditSystem.Workers.API.Interfaces.Services;
+
+namespace LisovaAuditSystem.Workers.API.Services;
+
+public class AuthenticationService(
+    IUserRepository userRepository,
+    IPasswordHasher passwordHasher,
+    JwtTokenGenerationService tokenGenerationService)
+    : IAuthenticationService
+{
+    public async Task<string> RegisterAsync(UserRegisterCredentialsDto userRegisterCredentials)
+    {
+        string passwordHash = passwordHasher.Hash(userRegisterCredentials.Password);
+
+        UserDto user =
+            new(
+                id: Guid.NewGuid(),
+                userName: userRegisterCredentials.UserName,
+                email: userRegisterCredentials.Email,
+                passwordHash: passwordHash);
+        await userRepository.AddAsync(user);
+
+        return tokenGenerationService.Generate(
+            userId: user.Id,
+            userName: user.UserName,
+            userEmail: user.Email);
+    }
+
+    public async Task<string> LoginAsync(UserLoginCredentialsDto userLoginCredentials)
+    {
+        UserDto? user = await userRepository.GetByEmailAsync(userLoginCredentials.Email);
+
+        if (user is null)
+        {
+            throw new InvalidOperationException($"User with email: {userLoginCredentials.Email} not found.");
+        }
+
+        bool isValidPassword = passwordHasher.Verify(userLoginCredentials.Password, user.PasswordHash);
+
+        if (!isValidPassword)
+        {
+            throw new InvalidOperationException("User password not valid!");
+        }
+
+        return tokenGenerationService.Generate(
+            userId: user.Id,
+            userName: user.UserName,
+            userEmail: user.Email);
+    }
+}
