92a66232ac7c3980b9c7262626fe57ae7f7b651f
Vitalii Barabash <vitalik.barabash.2002@gmail.com>
2025-02-15 15:15:12 +0200

    added transient JwtTokenGenerationService to collection

--- LisovaAuditSystem.Workers.API/Common/Extensions/DependencyInjections/BusinessServicesInjectionExtension.cs
+++ LisovaAuditSystem.Workers.API/Common/Extensions/DependencyInjections/BusinessServicesInjectionExtension.cs
@@ -1,5 +1,6 @@
 ﻿using LisovaAuditSystem.Workers.API.Interfaces.Services;
 using LisovaAuditSystem.Workers.API.Services;
+using LisovaAuditSystem.Workers.API.Services.Authentication;
 
 namespace LisovaAuditSystem.Workers.API.Common.Extensions.DependencyInjections;
 
@@ -8,5 +9,7 @@ public static class BusinessServicesInjectionExtension
     public static void AddBusinessServices(this IServiceCollection services)
     {
         services.AddScoped<IWorkerService, WorkerService>();
+
+        services.AddTransient<JwtTokenGenerationService>();
     }
 }
