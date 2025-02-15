bed928749ee839615a38f88ce281f76e7c1f1256
Vitalii Barabash <vitalik.barabash.2002@gmail.com>
2025-02-15 14:50:00 +0200

    defined and implemented JwtConfiguration model

---
+++ LisovaAuditSystem.Workers.API/Common/Configurations/JwtConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+
+namespace LisovaAuditSystem.Workers.API.Common.Configurations;
+
+public class JwtConfiguration
+{
+    public JwtConfiguration(IConfiguration configuration)
+    {
+        IConfigurationSection section = configuration.GetSection("JwtConfiguration");
+
+        Issuer = section[nameof(Issuer)] ?? string.Empty;
+        Audience = section[nameof(Audience)] ?? string.Empty;
+        Secret = section[nameof(Secret)] ?? string.Empty;
+        ExpireDays = int.Parse(section[nameof(ExpireDays)] ?? string.Empty, CultureInfo.InvariantCulture);
+    }
+
+    public string Issuer { get; }
+
+    public string Audience { get; }
+
+    public string Secret { get; }
+
+    public int ExpireDays { get; }
+}
